d51c890290ab6bfb7a3c156889e6bca4fe35c2e0
yuxiang.gao <yuxiang.gao@zhangmen.com>
2020-04-09 15:42:50 +0800

    处理增删改

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -2,19 +2,39 @@
   <div class="todo-wrapper">
     <h2>todos</h2>
     <div class="todo-content">
-      <el-input v-model="input" placeholder="try todo" @keyup.enter.native="handleEnter"></el-input>
-      <el-checkbox-group v-model="checkList" @change="handleCheckedChange">
-        <el-checkbox v-for="item in todoList" :key="item" :label="item">
-          <span>{{item}}</span>
-          <i class="el-icon-close" @click.prevent="hadleDelete(item)"></i>
+      <el-input v-model="input" placeholder="try to input todo" @keyup.enter.native="handleEnter"></el-input>
+      <el-checkbox-group v-model="checkList">
+        <el-checkbox
+          v-for="item in todoList"
+          :key="item.vaue"
+          :label="item.value"
+          v-show="selectedStatus === 0 || (selectedStatus === 1 ? !item.status : item.status)"
+          @change="handleCheckedChange($event, item)"
+        >
+          <span v-show="!item.isEdit">{{item.value}}</span>
+          <el-input
+            :ref="item.value"
+            type="text"
+            class="edit-input"
+            v-model="item.value"
+            v-show="item.isEdit"
+            @blur="handleEdit(item, false)"
+            @keyup.enter.native="handleEdit(item, false)"
+          />
+          <span>
+            <i class="el-icon-edit" @click.prevent="handleEdit(item, true)"></i>
+            <i class="el-icon-close" @click.prevent="hadleDelete(item.value)"></i>
+          </span>
         </el-checkbox>
       </el-checkbox-group>
-      <div class="info-list">
+      <div class="info-list" v-if="todoList.length > 0">
         <span>{{todoList.length - checkList.length}} items left</span>
         <span>
-          <el-button type="text" @click="handleAll">All</el-button>
-          <el-button type="text" @click="handleActived">Actived</el-button>
-          <el-button type="text" @click="handleCompleted">Completed</el-button>
+          <el-radio-group v-model="selectedStatus" size="mini">
+            <el-radio :label="0" border>All</el-radio>
+            <el-radio :label="1" border>Actived</el-radio>
+            <el-radio :label="2" border>Completed</el-radio>
+          </el-radio-group>
         </span>
         <span>
           <el-button type="text" @click="handleClear" v-if="checkList.length > 0">clear completed</el-button>
@@ -28,36 +48,42 @@
 export default {
   data () {
     return {
-      input: '',
-      todoList: [],
-      checkList: []
+      input: '', // 输入框的值
+      todoList: [], // 所有状态的列表
+      checkList: [], // 选中项列表
+      selectedStatus: 0, // 0: 全部  1: 激活  2: 已完成
     }
   },
   methods: {
+    // 回车增加事件
     handleEnter () {
       console.log('handleEnter')
-      this.todoList.push(this.input)
+      if (!this.input) return
+      this.todoList.push({ value: this.input, status: false, isEdit: false })
+      if (this.selectedStatus === 2) {
+        this.selectedStatus = 0
+      }
       this.input = ''
     },
+    // 删除事件
     hadleDelete (val) {
       console.log(val, 'hadleDelete')
-      this.todoList = this.todoList.filter(item => item !== val)
-
+      this.todoList = this.todoList.filter(item => item.value !== val)
     },
-    handleCheckedChange (value) {
+    // 监听选中改变事件
+    handleCheckedChange (value, item) {
       console.log(value, 'handleCheckedChange')
+      item.status = value
     },
-    handleAll () {
-      console.log('handleAll')
-    },
-    handleActived () {
-      console.log('handleActived')
-    },
-    handleCompleted () {
-      console.log('handleCompleted')
-    },
+    // 清空已完成
     handleClear () {
       console.log('handleClear')
+      this.todoList = this.todoList.filter(item => item.status !== true)
+    },
+    // 编辑事件
+    handleEdit (item, flag) {
+      console.log('handleEdit')
+      item.isEdit = flag
     }
   }
 }
@@ -77,12 +103,15 @@ export default {
     .el-checkbox-group {
       margin: 50px 0;
       text-align: left;
+      min-height: 200px;
     }
     .el-checkbox {
+      border-bottom: 1px dashed #e1e1e1;
       width: 100%;
       display: flex;
       justify-content: flex-start;
-      margin: 20px 0;
+      align-items: center;
+      padding: 20px 0;
       &.is-checked {
         .el-checkbox__label {
           span {
@@ -105,19 +134,45 @@ export default {
       display: flex;
       font-size: 20px;
       justify-content: space-between;
-      // padding: 0 20px;
+      align-items: center;
       width: 100%;
+    }
+    .edit-input {
+      input {
+        height: 22px;
+        line-height: 22px;
+      }
     }
     .el-icon-close {
       color: #333 !important;
+      margin-left: 12px;
       &:hover {
         color: #409eff !important;
       }
     }
     .info-list {
+      border-top: 1px solid #e1e1e1;
       display: flex;
       justify-content: space-between;
       line-height: 40px;
+      .el-radio-group {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 6px;
+        .el-radio {
+          margin-right: 0;
+        }
+      }
+      span {
+        flex: 1;
+        &:first-child {
+          text-align: left;
+        }
+        &:last-child {
+          text-align: right;
+        }
+      }
     }
   }
 }
